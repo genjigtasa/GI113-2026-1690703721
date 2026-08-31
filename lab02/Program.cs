b3f022c1e7617174bf12211c1fec05286ae61545
genjigtasa <genjigtasa05@gmail.com>
2026-08-31 21:30:50 +0700

    Add Lad02

---
+++ lab02/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Student ID : 1690703721
+ * Name       : Lab02
+ * Section    : 129D
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            // Lab02 Part A
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"AttackPower: {attackPower}");
+            Console.WriteLine($"CritMultiplier: {critMultiplier}");
+            Console.WriteLine($"IsBoss: {isBoss}");
+            Console.WriteLine();
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+        }
+    }
+}
